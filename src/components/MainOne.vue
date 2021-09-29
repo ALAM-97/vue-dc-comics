123888c2d9089a90d3afef1e1fce49c408ee6a02
ALAM-97 <alexamara97@gmail.com>
2021-09-29 17:04:05 +0200

    Main One Component Added

---
+++ src/components/MainOne.vue
@@ -0,0 +1,29 @@
+<template>
+<body>
+    <main class="container">
+        <h2> --> Content goes here </h2>
+    </main>
+</body>
+</template>
+
+<script>
+export default {
+    name:' MainOne',
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "../../style/common.scss";
+    @import "../../style/variables.scss";
+
+body {
+    background-color: $darkColor;
+    main {
+        margin: auto;
+        h2 {
+            color: #fff;
+            padding: 50px 0;
+        }
+    }
+}
+</style>
